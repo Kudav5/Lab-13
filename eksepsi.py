dc1f8711f29d4e9a055b9bbb3d8d26400443eb5c
Kudav5 <kdvalerian@gmail.com>
2022-12-14 00:08:05 +0700

    Lab 13

---
+++ eksepsi.py
@@ -0,0 +1,13 @@
+# readline untuk membaca per baris
+
+# tugasnya membuat readme isinya contoh dan penjelasan
+# try and except
+try:
+    var = 42
+    print(var/0)
+except :
+    print("except dijalankan ketika try salah")
+else:
+    print('else dijalankan ketika si try tidak salah')
+finally:
+    print('finally dijalankan ketika try dan except dijalankan')
